70bc515d4dd7ddae1277bbe2764a9ef2c3572c8c
Gabriel MMarinho <gabriel.jaka@gmail.com>
2025-02-22 03:45:42 +0000

    remove: arquivos e componentes não utilizados; adiciona configuração do Firebase adiciona realtime sync

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,47 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import { database, ref, get } from './config/firebase';
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      allCpfs: []
+    }
+  },
+
+  methods: {
+    async fetchAllCpfs() {
+      const cpfsRef = ref(database, 'cpfs');
+      const snapshot = await get(cpfsRef);
+      const data= snapshot.val()
+      this.allCpfs = data ? Object.values(data) : []
+      console.log(this.allCpfs)
+    }
+  },
+
+  created() {
+    this.fetchAllCpfs()
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>HelloWorld</h1>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
